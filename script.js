d60b2aacfff731927ec2eaaefe29acc35049de60
Liberbell <liberbell@gmail.com>
2019-11-28 08:02:10 +0900

    script4

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-var date = new Date();
-document.body.innerHTML = "<h1>Today is: " + date + "</h1>";
+// var date = new Date();
+// document.body.innerHTML = "<h1>Today is: " + date + "</h1>";
+
+var a = 4;
+var b = "5";
+var sum = a + b;
