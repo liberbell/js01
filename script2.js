7c76d1d8efa1e62fac077cba43145d8e492d6305
Liberbell <liberbell@gmail.com>
2019-11-29 07:47:51 +0900

    script2-2

---
+++ script2.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 5;
+var theNumberMatch;
+
+if (a == b) {
+  theNumberMatch = true;
+} else {
+  theNumberMatch = false;
+}
+
+console.log("the numbers match: " + theNumberMatch);
