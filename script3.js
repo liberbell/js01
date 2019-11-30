cae2f7636cc374980bfa9046dad7ee83691a6bb5
Liberbell <liberbell@gmail.com>
2019-11-30 18:14:50 +0900

    script3-5

--- script3.js
+++ script3.js
@@ -2,5 +2,9 @@ var pens;
 pens = ["red", "blue", "green", "orange"];
 // pens = new Array("red", "blue", "green", "orange", "purple")
 
-var mixedBag = ["key", 5, false, 54.3, "one more thing"]
-console.log(mixedBag);
+// var mixedBag = ["key", 5, false, 54.3, "one more thing"]
+console.log(pens);
+
+var fourthPen = pnes[3];
+
+console.log(fourthPen)
