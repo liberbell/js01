717cc198cedf98e3de4a9591ff11849a4fba8247
Liberbell <liberbell@gmail.com>
2019-11-30 18:11:00 +0900

    script3-3

--- script3.js
+++ script3.js
@@ -1,4 +1,4 @@
 var pens;
 // pens = ["red", "blue", "green", "orange"];
-pens = new Array("red", "blue", "green", "orange")
+pens = new Array("red", "blue", "green", "orange", "purple")
 console.log(pens);
