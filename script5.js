1497937fb49e14236c2a85026f98a1caa0b9617d
Liberbell <liberbell@gmail.com>
2019-12-02 07:44:16 +0900

    script5-3

--- script5.js
+++ script5.js
@@ -3,3 +3,9 @@ function multiply() {
   console.log("3 multiplied by 4 is: ", result);
 }
 multiply();
+
+var divided() {
+  var result = 3/ 4;
+  console.log("3 divided by 4 is: ", result);
+}
+divided()
