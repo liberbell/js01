b3ad513e4020481d943409243cab11d8654e7672
Liberbell <liberbell@gmail.com>
2019-12-03 11:04:25 +0900

    script6-3

--- script6.js
+++ script6.js
@@ -1,5 +1,10 @@
 function findBiggestFraction() {
-  console.log("The function is running!");
+  if a > b ? console.log("a: ", a) : console.log("b: ", b);
+  // console.log("The function is running!");
 }
+
+var a = 3 / 4;
+var b = 5 / 7;
+
 
 findBiggestFraction();
