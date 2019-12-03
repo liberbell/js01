8136657803c2711ef29914769f7fa26ecb7c6266
Liberbell <liberbell@gmail.com>
2019-12-04 07:46:04 +0900

    script62-3

--- script62.js
+++ script62.js
@@ -3,3 +3,8 @@ function findBiggestFraction(a,b) {
   a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
   return result;
 }
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+console.log(findBiggestFraction(firstFraction, secondFraction));
