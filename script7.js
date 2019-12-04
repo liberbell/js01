1841ea1181c110a4eaeeca8321a923642914af95
Liberbell <liberbell@gmail.com>
2019-12-05 07:41:50 +0900

    script7-6

--- script7.js
+++ script7.js
@@ -1,11 +1,11 @@
 var a = 5 / 7;
 var b = 18 / 25;
 
-var theBiggest = function() {
+var theBiggest = function(a, b) {
   var result;
   a > b ? result = ["a", a] : result = ["b", b];
   // console.log(result);
   return result;
 }
 
-console.log(theBiggest());
+console.log(theBiggest(101, 20));
