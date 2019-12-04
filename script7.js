e8b45b234d57b49942b782aeaf87d91067eb5e16
Liberbell <liberbell@gmail.com>
2019-12-05 07:49:25 +0900

    script7-9

--- script7.js
+++ script7.js
@@ -1,5 +1,5 @@
-var a = 5 / 7;
-var b = 18 / 25;
+var firstFraction = 7 /9;
+var secondFraction = 13 /29;
 
 var theBiggest = (function(a, b) {
   var result;
@@ -8,8 +8,7 @@ var theBiggest = (function(a, b) {
   return result;
 })(firstFraction, secondFraction)
 
-var firstFraction = 7 /9;
-var secondFraction = 13 /29;
+
 
 console.log(theBiggest);
 // console.log(theBiggest);
