eee10d5e9a5d3a1b737ee3bd959b1060aeeb407e
Liberbell <liberbell@gmail.com>
2019-12-06 07:35:55 +0900

    script8-2

---
+++ script8.js
@@ -0,0 +1,9 @@
+function findBiggestFraction(a, b) {
+  console.log("Fraction a: ", firstFraction);
+  console.log("Fraction b: ", secondFraction);
+
+  var result;
+
+  a>b ? result = ["a", a] : result = ["b", b];
+  return result;
+}
