95b767c1f710a034ef24c0f38fa0c278a5482de5
Liberbell <liberbell@gmail.com>
2019-12-07 14:48:50 +0900

    script10-5

--- script10.js
+++ script10.js
@@ -1,17 +1,17 @@
 var course = new Object();
 
-course.title = "Java script Essential Traning.";
-course.instructor = "Samuel jackson";
-course.level = 1;
-course.published = true;
-course.views = 0;
+// course.title = "Java script Essential Traning.";
+// course.instructor = "Samuel jackson";
+// course.level = 1;
+// course.published = true;
+// course.views = 0;
 
-// var course = {
-//   title = "Java script Essential Traning.";
-//   instructor = "Samuel jackson";
-//   level = 1;
-//   published = true;
-//   views = 0;
-// }
+var course = {
+  title = "Java script Essential Traning.",
+  instructor = "Samuel jackson",
+  level = 1,
+  published = true,
+  views = 0,
+}
 
 console.log(course)
