cdfe497c4adf78cb5bcdb209cda73d9daf4d2d66
Liberbell <liberbell@gmail.com>
2019-12-08 11:53:37 +0900

    script11-2

---
+++ script11.js
@@ -0,0 +1,7 @@
+function Course(title, instructor, level, published, views) {
+  this.title = title;
+  this.instructor = instructor;
+  this.level = level;
+  this published = published;
+  this.views = views;
+}
