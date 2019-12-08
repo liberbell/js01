97bdf105bcfc4dc67ec20056cb29a8a5b1adc900
Liberbell <liberbell@gmail.com>
2019-12-08 12:01:41 +0900

    script11-6

--- script11.js
+++ script11.js
@@ -10,4 +10,7 @@ function Course(title, instructor, level, published, views) {
 }
 
 var course01 = new Course("Java script Essential Traning.", "Samuel jackson", 1, true, 0);
+var course02 = new Course("Up and running.", "Taylor swift", 2, true, 100);
+
 console.log(course01);
+console.log(course02);
