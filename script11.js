f33e7f03c0b7f4bc686e01b5e89c8fe55322a5df
Liberbell <liberbell@gmail.com>
2019-12-08 11:58:05 +0900

    script11-5

--- script11.js
+++ script11.js
@@ -6,5 +6,7 @@ function Course(title, instructor, level, published, views) {
   this.views = views;
   this.updateViews = function() {
     return ++this.views;
-  }
+  };
 }
+
+var course01 = new Course("Java script Essential Traning.", "Samuel jackson", 1, true, 0);
