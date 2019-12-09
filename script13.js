340d8d27fa245ecd1acd44526c1f76e39a279dfd
Liberbell <liberbell@gmail.com>
2019-12-10 07:37:17 +0900

    script13-3

--- script13.js
+++ script13.js
@@ -13,7 +13,7 @@ var mediumSize = giveMeEms(18);
 var largeSize = giveMeEms(24);
 var xlargeSize = giveMeEms(32);
 
-console.log("Small size: ", smallSize);
-console.log("Medium size: ", mediumSize);
-console.log("Large size: ", largeSize);
-console.log("Xlarge size: ", xlargeSize);
+console.log("Small size: ", smallSize());
+console.log("Medium size: ", mediumSize());
+console.log("Large size: ", largeSize());
+console.log("Xlarge size: ", xlargeSize());
