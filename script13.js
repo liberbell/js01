9430f8bd2c6d431cf540689deccda6ba6cb62fa3
Liberbell <liberbell@gmail.com>
2019-12-10 07:36:12 +0900

    script13-2

--- script13.js
+++ script13.js
@@ -8,4 +8,12 @@ function giveMeEms(pixels) {
   return doSomeMath;
 }
 
-var smallSize = giveMeEms(12); 
+var smallSize = giveMeEms(12);
+var mediumSize = giveMeEms(18);
+var largeSize = giveMeEms(24);
+var xlargeSize = giveMeEms(32);
+
+console.log("Small size: ", smallSize);
+console.log("Medium size: ", mediumSize);
+console.log("Large size: ", largeSize);
+console.log("Xlarge size: ", xlargeSize);
