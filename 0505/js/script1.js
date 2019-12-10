79949ca82e90dae49949e81c94241876454736af
Liberbell <liberbell@gmail.com>
2019-12-11 07:53:55 +0900

    script1-1

--- 0505/js/script1.js
+++ 0505/js/script1.js
@@ -1,3 +1,7 @@
 const CTAELEMENT = document.querySelector(".cta a");
 
+if (CTAELEMENT.hasAttribute("target")) {
+  console.log(CTAELEMENT.getAttribute("target"));
+}
+
 console.log( CTAELEMENT.attributes );
