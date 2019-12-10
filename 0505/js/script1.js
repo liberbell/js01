a348ffed00ff91f29ca7f33a3f3cd83360d0f582
Liberbell <liberbell@gmail.com>
2019-12-11 07:56:08 +0900

    script1-2

--- 0505/js/script1.js
+++ 0505/js/script1.js
@@ -3,7 +3,7 @@ const CTAELEMENT = document.querySelector(".cta a");
 if (CTAELEMENT.hasAttribute("target")) {
   console.log(CTAELEMENT.getAttribute("target"));
 } else {
-  CTAELEMENT.setAttribute("target", "_blank")
+  CTAELEMENT.setAttribute("target", "_blank");
 }
 
 console.log( CTAELEMENT.attributes );
