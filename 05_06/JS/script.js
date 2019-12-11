268c58001a7404857c58aab41fcc48360311e293
Liberbell <liberbell@gmail.com>
2019-12-12 07:45:28 +0900

    script4

--- 05_06/JS/script.js
+++ 05_06/JS/script.js
@@ -4,3 +4,7 @@ const THEIMAGE = FEATURED.querySelector("img");
 
 var altText = THEIMAGE.getAttribute("alt");
 var captionElement = document.createElement("figcaption");
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
+console.log(captionElement);
