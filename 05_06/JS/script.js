7ea0f1943fd4bf03876e133cae6322b01aaf58fb
Liberbell <liberbell@gmail.com>
2019-12-12 07:43:47 +0900

    script3

--- 05_06/JS/script.js
+++ 05_06/JS/script.js
@@ -1,3 +1,6 @@
 // console.log("test")
 const FEATURED = document.querySelector(".featured-image");
 const THEIMAGE = FEATURED.querySelector("img");
+
+var altText = THEIMAGE.getAttribute("alt");
+var captionElement = document.createElement("figcaption");
