f4f55e766dc97f64ffc9d34a5ceeeea6d889972c
Liberbell <liberbell@gmail.com>
2019-12-13 08:00:27 +0900

    script5

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,16 +2,16 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
-
 var date = new Date();
 console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: "+ min + " Second: " + sec)
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = sec / 60 * 360;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
