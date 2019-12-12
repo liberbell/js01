bd3c43bff7c32ae45614ec56cedeed4d4bb17f69
Liberbell <liberbell@gmail.com>
2019-12-13 07:44:20 +0900

    script2

--- 06_02/script.js
+++ 06_02/script.js
@@ -1,3 +1,7 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
