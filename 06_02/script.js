812eba2dd91b81193a644d0d1b7ab4022d979f15
Liberbell <liberbell@gmail.com>
2019-12-13 07:46:06 +0900

    script3

--- 06_02/script.js
+++ 06_02/script.js
@@ -5,3 +5,7 @@ const SECONDHAND = document.querySelector("#second");
 let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
