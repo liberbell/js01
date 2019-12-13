a67d54fe56fb94148ed6af223ba0676615eb0981
Liberbell <liberbell@gmail.com>
2019-12-14 07:30:17 +0900

    script7

--- 06_02/script.js
+++ 06_02/script.js
@@ -14,6 +14,10 @@ let minPosition = min / 60 * 360 + (sec*(360/60)/60);
 let secPosition = sec / 60 * 360;
 
 function runTheClock() {
+
+  minPosition = minPosition + (6/60);
+  secPosition = secPosition + 6;
+  
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
