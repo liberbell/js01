6361287ec4c13e595afa298edc9cea263f54b7a1
Liberbell <liberbell@gmail.com>
2019-12-16 07:44:27 +0900

    script3

--- 07_05/JS/script.js
+++ 07_05/JS/script.js
@@ -5,10 +5,11 @@ CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
 function reveal(current) {
-    e.preventDefault();
-    CTA.classList.toggle("hide");
+    // e.preventDefault();
+    // CTA.classList.toggle("hide");
+    current.innerHTML == "Book Now!" ? CTA.innerHTML == "Oooops!" : CTA.innerHTML == "Book Now!";
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener('click', reveal, false);
+CTA.addEventListener('click', function() { reveal(this); }, false);
 CTA.addEventListener('click', function(){console.log("The button was clicked!")}, false);
