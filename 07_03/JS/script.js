f1b9a48d082e84c1f88dc10ab9eae88f2c6af56e
Liberbell <liberbell@gmail.com>
2019-12-15 14:00:02 +0900

    script2

--- 07_03/JS/script.js
+++ 07_03/JS/script.js
@@ -2,4 +2,5 @@
 const CTA = document.querySelector(" .cta a");
 const ALERT = document.querySelector("#booking-alert");
 
-// CTA.classList.remove("hide");
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
