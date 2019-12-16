5c74b2900beb2ff34d9e8545b01782f38e1213f0
Liberbell <liberbell@gmail.com>
2019-12-17 07:54:11 +0900

    script8

--- 08_03/script.js
+++ 08_03/script.js
@@ -15,6 +15,10 @@ function runTimeer() {
   let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2] + ":";
   theTimer.innerHTML = currentTime;
   timer[3]++;
+
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 
