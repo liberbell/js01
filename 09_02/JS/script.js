31832449b29abfb635180822352aaed5c27796cf
Liberbell <liberbell@gmail.com>
2019-12-20 07:37:23 +0900

    script2

--- 09_02/JS/script.js
+++ 09_02/JS/script.js
@@ -2,5 +2,8 @@ var extLinks = document.querySelectorAll('a[href^="http"]');
 // console.log(extLinks);
 
 for (var i = 0; i < extLinks.length; i++) {
-  console.log(extLinks);
+  // console.log(extLinks);
+  if (!extLinks[i].hasAttribute("target") ) {
+    extLinks[i].setAttribute("target", "_blank");
+  }
 }
