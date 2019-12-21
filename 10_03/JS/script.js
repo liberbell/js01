12f58800b36d3ded511df79826767a17953b3bda
Liberbell <liberbell@gmail.com>
2019-12-21 11:50:10 +0900

    script3

--- 10_03/JS/script.js
+++ 10_03/JS/script.js
@@ -2,5 +2,6 @@ const IMAGES = document.querySelectorAll("img");
 
 for (let i = 0; i < IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute("src");
+  imgSrc = imgSrc.slice(0, -8);
   console.log(imgSrc);
 }
