2d68454676434ea232b954cdbdedab41ccf6360f
Liberbell <liberbell@gmail.com>
2019-12-21 11:51:55 +0900

    script3

--- 10_03/JS/script.js
+++ 10_03/JS/script.js
@@ -4,4 +4,7 @@ for (let i = 0; i < IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute("src");
   imgSrc = imgSrc.slice(0, -8);
   console.log(imgSrc);
+
+  let type = IMAGES[i].getAttribute("date-type");
+  console.log(type)
 }
