8833660bcd08f1517679be3c2e85237e8d4c1e19
Liberbell <liberbell@gmail.com>
2019-12-22 18:50:19 +0900

    script5

--- 10_03/JS/script.js
+++ 10_03/JS/script.js
@@ -15,7 +15,7 @@ for (let i = 0; i < IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute("src");
   imgSrc = imgSrc.slice(0, -8);
   let srcset = makeSrcset(imgSrc);
-  console.log(imgSrc);
+  console.log(srcset);
 
   let type = IMAGES[i].getAttribute("data-type");
   console.log(type);
